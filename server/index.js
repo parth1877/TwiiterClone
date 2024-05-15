d7c412d09f14bdb5caa7e4cfb1b7b6404020ea0a
parth1877 <141398887+parth1877@users.noreply.github.com>
2024-05-16 00:14:21 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ DB_Connect();
 app.use(express.json());
 app.use(cookieParser());    
 const corsOptions = {
-    origin:"http://localhost:3000",
+    origin:"https://twiter-clone-ivory.vercel.app",
     credentials:true
 }
 

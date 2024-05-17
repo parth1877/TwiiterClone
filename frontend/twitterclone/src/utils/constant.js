3d8e1f17d4ef5f381c40257a7194e8251a03a2fd
parth1877 <141398887+parth1877@users.noreply.github.com>
2024-05-17 11:37:48 +0530

    Update constant.js

--- frontend/twitterclone/src/utils/constant.js
+++ frontend/twitterclone/src/utils/constant.js
@@ -1,2 +1,2 @@
-export const User_API_Endpoint = "https://twiter-cloneserver.vercel.app/api/v1/user"
-export const Tweet_API_Endpoint = "https://twiter-cloneserver.vercel.app/api/v1/tweet"
+export const User_API_Endpoint = "https://twitercloneserver.onrender.com/api/v1/user"
+export const Tweet_API_Endpoint = "https://twitercloneserver.onrender.com/api/v1/tweet"
